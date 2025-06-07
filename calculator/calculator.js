921f25e48e6d78fab17fe0aa11d91f71b99c2831
binit2-1 <binitgupta.1711@gmail.com>
2025-06-08 00:37:40 +0530

    Now buttons will display

---
+++ calculator/calculator.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.querySelector(".display");
+    const buttons = document.querySelectorAll(".btn");
+    const equal = document.querySelector(".btneq");
+    const clear = document.querySelector(".btnc");
+
+    function displayValue(value){
+        display.textContent += value   
+    }
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () =>
+        {   display.style.color = "white";
+            display.style.textAlign = "right";
+            if(button.classList.contains("btnc")){
+                display.textContent = "";        
+            }
+            else if(button.classList.contains("btnd")){
+                display.textContent = display.textContent.slice(0, -1)
+            }
+            else{
+                displayValue(button.textContent);
+            }
+        });
+    });
+});
