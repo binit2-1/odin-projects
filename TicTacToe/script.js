10f0800096578be0830a2b4a47363156cad3698a
binit <binitgupta.1711@gmail.com>
2025-07-06 18:10:16 +0530

    The dialogue box taking radio box input and console logging'

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dialog = document.querySelector('dialog');
     dialog.showModal();
+    const start =  document.querySelector('.start-game');
+    const humanCheckbox = document.getElementById('Human');
+    const computerCheckbox = document.getElementById('Computer');
+    start.addEventListener('click', ()=>{
+        const playerType = humanCheckbox.checked ? 'Human' : 'Computer';
+        dialog.close();
+        console.log(`Selected player type: ${playerType}`);
+    })
 });
