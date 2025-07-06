be9d6cb551742c86082b9d24a4efcfe3a11d8f76
binit <binitgupta.1711@gmail.com>
2025-07-06 18:29:08 +0530

    After selecting opponent it renders the opponent type under the game name

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dialog = document.querySelector('dialog');
-    dialog.showModal();
+    
     const start =  document.querySelector('.start-game');
     const humanCheckbox = document.getElementById('Human');
     const computerCheckbox = document.getElementById('Computer');
+    const dialog = document.querySelector('dialog');
+    dialog.showModal();
 
     dialog.addEventListener('cancel', (event) => {
         event.preventDefault();
@@ -13,5 +14,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const playerType = humanCheckbox.checked ? 'Human' : 'Computer';
         dialog.close();
         console.log(`Selected player type: ${playerType}`);
+        if (playerType === 'Human'){
+        document.querySelector('.vsHuman').textContent = 'YOU vs HUMAN';
+        document.querySelector('.vsComputer').textContent = '';
+        }
+
+        else if (playerType === 'Computer'){
+            document.querySelector('.vsComputer').textContent = 'YOU vs COMPUTER';
+            document.querySelector('.vsHuman').textContent = '';
+        }
     })
 });
